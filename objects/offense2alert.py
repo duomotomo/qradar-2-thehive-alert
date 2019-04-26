15d3bbc50f19b12854f08465a5a8211f2587846d
Dmitry Tomko <dtomko@ibagroup.eu>
2019-04-26 17:09:52 +0300

    Update offense2alert.py

--- objects/offense2alert.py
+++ objects/offense2alert.py
@@ -183,7 +183,7 @@ def allOffense2Alert():
                 try:
                     theHiveAlert = qradarOffenseToHiveAlert(theHiveConnector, enrichedOffense)
                     # theHiveEsAlertId = theHiveConnector.createAlert(theHiveAlert)['id']
-                    offense_report['raised_alert_id'] = "12" #theHiveEsAlertId
+                    offense_report['raised_alert_id'] = theHiveEsAlertId
                     offense_report['qradar_offense_id'] = offense['id']
                     offense_report['success'] = True
                     if offenseLastId < offense['id']:
