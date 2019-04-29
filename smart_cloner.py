aacd35ce46e4f94fd3565c6bce6513e148d4297d
Aliaksandar Vertsiy <avertsiy@iba.by>
2019-04-29 15:21:38 +0300

    qradarapps/qradar2thehive#1

--- smart_cloner.py
+++ smart_cloner.py
@@ -21,24 +21,25 @@ logger.info('%s.SMART_CLONNER', __name__)
 
 cfg = getConf()
 
-cfg['smartclonner']['status'] = str(10)
+scStatus = cfg['smartclonner']['status']
+logger.info("smartclonner status: " + scStatus)
 
-setConf(cfg)
-
-# scStatus = cfg['smartclonner']['status']
-# logger.info("smartclonner status: " + scStatus)
-
-# if int(scStatus) == 0:
-#         cfg['smartclonner']['status'] = str(1)
-#         setConf(cfg)
-#         logger.info("launching clonning offenses as alert")
-#         report = allOffense2Alert()
-#         for reportOffense in report['offenses']:
-#                 logger.info("Is offense " + str(reportOffense['qradar_offense_id']) + \
-#                                 " clonned to alert " + str(reportOffense['raised_alert_id']) + \
-#                                 " : " + str(reportOffense['success']))
-#         cfg = getConf()
-#         cfg['smartclonner']['status'] = str(0)
-#         setConf(cfg)
-# else:
-#         logger.info("another instance of smartclonner has been launched")
+if int(scStatus) == 0:
+        try:
+                cfg['smartclonner']['status'] = str(1)
+                setConf(cfg)
+                logger.info("launching clonning offenses as alert")
+                report = allOffense2Alert()
+                for reportOffense in report['offenses']:
+                        logger.info("Is offense " + str(reportOffense['qradar_offense_id']) + \
+                                        " clonned to alert " + str(reportOffense['raised_alert_id']) + \
+                                        " : " + str(reportOffense['success']))
+        except Exception as ex:
+                logger.error("Tool exception: " + str(ex))
+        
+        # set status to "completed"
+        cfg = getConf()
+        cfg['smartclonner']['status'] = str(0)
+        setConf(cfg)
+else:
+        logger.info("another instance of smartclonner has been launched")
